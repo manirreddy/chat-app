74c88439939e0fc9f748cfd3968a84ae07f7fc04
manirreddy <manireddy2652@gmail.com>
2025-06-30 13:56:50 +0530

    Update script.js

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ const messages = document.getElementById('messages');
 
 form.addEventListener('submit', function (e) {
   e.preventDefault();
-  if (input.value) {
+  if (input.value.trim()) {
     socket.emit('chat message', input.value);
     input.value = '';
   }
