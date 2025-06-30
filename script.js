dbb9ff9754045c85174fa840322360bb11ebb6ff
manirreddy <manireddy2652@gmail.com>
2025-06-30 13:44:15 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const socket = io("wss://a99ea844-b5cd-4c26-b215-26a30a2742d0-00-dgysv1t6dgvg.sisko.replit.dev", {
+const socket = io("https://a99ea844-b5cd-4c26-b215-26a30a2742d0-00-dgysv1t6dgvg.sisko.replit.dev", {
   transports: ["websocket"],
   withCredentials: false
 });
@@ -7,7 +7,7 @@ const form = document.getElementById('form');
 const input = document.getElementById('input');
 const messages = document.getElementById('messages');
 
-form.addEventListener('submit', (e) => {
+form.addEventListener('submit', function (e) {
   e.preventDefault();
   if (input.value) {
     socket.emit('chat message', input.value);
@@ -15,9 +15,8 @@ form.addEventListener('submit', (e) => {
   }
 });
 
-socket.on('chat message', (msg) => {
+socket.on('chat message', function (msg) {
   const item = document.createElement('li');
   item.textContent = msg;
   messages.appendChild(item);
-  window.scrollTo(0, document.body.scrollHeight);
 });
